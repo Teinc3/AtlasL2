07879f0a209de93a2e8548b6246e3918f007ff04
Teinc3 <teinc3@gmail.com>
2026-03-23 23:58:52 +0000

    Added Golden borders geojson layer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Map, useControl } from "react-map-gl/maplibre";
 import { MapboxOverlay } from '@deck.gl/mapbox';
+import { GeoJsonLayer } from '@deck.gl/layers';
 
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './App.css'
@@ -18,6 +19,19 @@ function DeckGLOverlay(props: MapboxOverlayProps) {
 export default function App() {
   const [hasActiveData, _] = useState(false);
 
+  const layers = [
+    new GeoJsonLayer({
+      id: 'golden-border-layer',
+      data: 'https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/ne_50m_admin_0_countries.geojson',
+      stroked: true,
+      lineWidthUnits: 'pixels',
+      getLineWidth: 1,
+      getLineColor: [255, 215, 0, 200],
+      lineWidthMinPixels: 0.25,
+      beforeId: 'place_continent',
+    })
+  ]
+
   return (
     <div className="mapContainer">
       <Map
@@ -25,10 +39,12 @@ export default function App() {
         initialViewState={{
           longitude: 22,
           latitude: 27,
-          zoom: 1.8
+          zoom: 1.8,
+          pitch: 0
         }}
+        bearing={0}
       >
-        <DeckGLOverlay layers={[]} interleaved />
+        <DeckGLOverlay layers={layers} interleaved />
       </Map>
     </div>
   )
