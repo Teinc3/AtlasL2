98e72c629874277f7c14f47346294fd49bd45c1c
Teinc3 <teinc3@gmail.com>
2026-03-29 17:13:40 +0100

    Restore base map borders and labels

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { MapboxOverlay } from '@deck.gl/mapbox';
 
 import mockLinguisticProfiles from "./api";
 import { SelectPanel, InfoPanel, HoverPanel } from "./components/panels";
-import { getCommunicabilityColor, getElevation, useMapInteractions, useAtlasContext } from "./utils";
+import { getCommunicabilityColor, getElevation, useMapInteractions } from "./utils";
 
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './App.css'
@@ -23,8 +23,6 @@ function DeckGLOverlay(props: MapboxOverlayProps) {
 
 export default function App() {
   const mapRef = useRef<MapRef>(null);
-  const { selectedLanguages, selectedCountries } = useAtlasContext();
-  const hasActiveData = selectedLanguages.length > 0 || selectedCountries.length > 0;
 
   const {
     hoverInfo,
@@ -36,42 +34,28 @@ export default function App() {
     closeHoverPanel
   } = useMapInteractions(mapRef);
 
-  const layers = useMemo(() => [
-    new GeoJsonLayer<CountryFeatureProperties>({
-      id: 'country-fill-layer',
-      data: 'https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/ne_50m_admin_0_countries.geojson',
-      filled: true,
-      stroked: false,
-      extruded: true,
-      wireframe: false,
-      getFillColor: getCommunicabilityColor,
-      getElevation,
-      autoHighlight: true,
-      highlightColor: [255, 255, 255, 50],
-      pickable: true,
-      onHover: handleHover,
-      onClick: handleClick
-    }),
-    new GeoJsonLayer<CountryFeatureProperties>({
-      id: 'golden-wall-layer',
-      data: 'https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/ne_50m_admin_0_countries.geojson',
-      filled: false,
-      stroked: true,
-      extruded: false,
-      wireframe: false,
-      getLineColor: [255, 215, 0, 220],
-      lineWidthUnits: 'pixels',
-      getLineWidth: 1,
-      lineWidthMinPixels: 0.6,
-    })
-  ], [mockLinguisticProfiles, handleHover, handleClick]);
+  const layers = useMemo(() => [new GeoJsonLayer<CountryFeatureProperties>({
+    id: 'country-fill-layer',
+    data: 'https://raw.githubusercontent.com/nvkelso/natural-earth-vector/master/geojson/ne_50m_admin_0_countries.geojson',
+    filled: true,
+    stroked: false,
+    extruded: true,
+    wireframe: false,
+    getFillColor: getCommunicabilityColor,
+    getElevation,
+    autoHighlight: true,
+    highlightColor: [255, 255, 255, 50],
+    pickable: true,
+    onHover: handleHover,
+    onClick: handleClick
+  })], [mockLinguisticProfiles, handleHover, handleClick]);
 
   return (
     <div className="appContainer">
       <div className="mapContainer">
         <Map
           ref={mapRef}
-          mapStyle={hasActiveData ? '/basemap-data.style.json' : '/basemap-context.style.json'}
+          mapStyle={'/basemap-data.style.json'}
           initialViewState={{
             longitude: 22,
             latitude: 27,
